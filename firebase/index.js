207600304939f8999df3252cda5671fe625208d4
kartos-1993 <1993kartos@gmail.com>
2023-03-14 21:58:09 +0545

    initialize firebase App with application default fun

--- firebase/index.js
+++ firebase/index.js
@@ -3,7 +3,7 @@ var admin = require("firebase-admin");
 const serviceAccount = require("../config/firebaseServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault(),
   databaseURL: "https://celebs-db-3371a.firebaseio.com",
 });
 
