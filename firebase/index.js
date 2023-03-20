536f594478f9029657100c2538ab3a97e9b01bad
kartos-1993 <1993kartos@gmail.com>
2023-03-20 23:26:13 +0545

    fix firebase admin sdk credential for produciton

--- firebase/index.js
+++ firebase/index.js
@@ -2,15 +2,21 @@ var admin = require("firebase-admin");
 require("dotenv").config();
 
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https://celebs-db-3371a.firebaseio.com",
-});
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://celebs-db-3371a.firebaseio.com",
+  });
+} else {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://celebs-db-3371a.firebaseio.com",
+  });
+}
 
+module.exports = admin;
 
- module.exports = admin
-
-
-//  {"err":{"code":"auth/id-token-expired","message":"Firebase ID token has expired. 
+//  {"err":{"code":"auth/id-token-expired","message":"Firebase ID token has expired.
 //  Get a fresh ID token from your client app and try again (auth/id-token-expired).
 //   See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token."}}
