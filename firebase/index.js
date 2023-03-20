ad3d30e7277fb7dfe716ba066753a8495709a6cd
kartos-1993 <1993kartos@gmail.com>
2023-03-21 02:04:39 +0545

    edit firebase admin sdk

--- firebase/index.js
+++ firebase/index.js
@@ -5,7 +5,7 @@ const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://celebs-db-3371a.firebaseio.com",
   });
   
