62c8ee79a3922a728e4c8149544bb3a52fec48f4
kartos-1993 <1993kartos@gmail.com>
2023-03-21 01:46:29 +0545

    edit firebase admin credential

--- firebase/index.js
+++ firebase/index.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-require("dotenv").config();
+
 
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
@@ -8,12 +8,8 @@ const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
     credential: admin.credential.applicationDefault(),
     databaseURL: "https://celebs-db-3371a.firebaseio.com",
   });
-
   
 
 
 module.exports = admin;
 
-//  {"err":{"code":"auth/id-token-expired","message":"Firebase ID token has expired.
-//  Get a fresh ID token from your client app and try again (auth/id-token-expired).
-//   See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token."}}
