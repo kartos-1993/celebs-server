45b83e02f8fd4643cd9ff59b5fbfdcd40d537819
kartos-1993 <1993kartos@gmail.com>
2023-03-21 01:11:53 +0545

    edit firebase admin sdk credentials

--- firebase/index.js
+++ firebase/index.js
@@ -2,19 +2,13 @@ var admin = require("firebase-admin");
 require("dotenv").config();
 
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-if(process.env.NODE_ENV === "production"){
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: "https://celebs-db-3371a.firebaseio.com"
-  })
-}else{
-  console.log("if run i am for development")
+
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://celebs-db-3371a.firebaseio.com",
   });
-}
+
   
 
 
