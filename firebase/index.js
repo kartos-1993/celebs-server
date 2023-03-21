8ba5209069e0fa4d4a116c81f99f44ac7c20f711
kartos-1993 <1993kartos@gmail.com>
2023-03-21 21:42:19 +0545

    fix firebase admin sdk credential

--- firebase/index.js
+++ firebase/index.js
@@ -1,29 +1,20 @@
 var admin = require("firebase-admin");
 
-const account = require("../config/firebaseServiceAccountKey.json")
-
-const a = process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n");
-
-// private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
-
-
-const serviceAccount = {
-  type: "service_account",
-  project_id: "celebs-db-3371a",
-  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: a,
-  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
-};
-
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: "celebs-db-3371a",
+    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+  }),
   databaseURL: "https://celebs-db-3371a.firebaseio.com",
 });
 
