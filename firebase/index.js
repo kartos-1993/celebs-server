813f5a6977ea3d24d467ab52671fbcb7a3aadef7
kartos-1993 <1993kartos@gmail.com>
2023-03-21 16:49:31 +0545

    fix firebase admin sdk config

--- firebase/index.js
+++ firebase/index.js
@@ -1,12 +1,17 @@
 var admin = require("firebase-admin");
 
-// const a = process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n");
+const account = require("../config/firebaseServiceAccountKey.json")
+
+const a = process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+// private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
+
 
 const serviceAccount = {
   type: "service_account",
-  project_id: "your-project-id",
+  project_id: "celebs-db-3371a",
   private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: a,
   client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -15,6 +20,7 @@ const serviceAccount = {
     process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
 };
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
